5846a13ca381dbb2758a08627228722a25b6f0a7
tirsomartinezreyes <tirsomartinezreyes@gmail.com>
2021-11-02 02:07:39 -0600

    labels connected with sliders

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightSliderIBOutlet: UISlider!
+    @IBOutlet weak var heightLabelIBOutlet: UILabel!
+    @IBOutlet weak var weightSliderIBOutlet: UISlider!
+    @IBOutlet weak var weightLabelIBOutlet: UILabel!
+    
+    @IBAction func onHeightChanged(_ sender: UISlider, forEvent event: UIEvent) {
+        print(heightSliderIBOutlet.value)
+        updateUI()
+    }
+    
+    
+    @IBAction func onWeightChanged(_ sender: UISlider, forEvent event: UIEvent) {
+        print(weightSliderIBOutlet.value)
+        updateUI()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func updateUI(){
+        heightLabelIBOutlet.text = String(format:"%.2f", heightSliderIBOutlet.value) + "m"
+        weightLabelIBOutlet.text = String(format:"%.0f",weightSliderIBOutlet.value) + "K"
     }
 
 
